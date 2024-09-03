4c6cdcca35c3e1e37bc9c3e4a16116cc3de90d59
pahipy <pahipy@gmail.com>
2024-09-04 00:15:37 +0300

    wpf window was added

---
+++ Editor/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace SText.Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FluentWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApplicationThemeManager.Apply(this);
+        }
+    }
+}
