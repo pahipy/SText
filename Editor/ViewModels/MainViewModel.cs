6bbcc8fba29dd6311c1a531bdc35e9145e3b4379
pahipy <pahipy@gmail.com>
2024-09-10 20:25:03 +0300

    viewmodel was added

---
+++ Editor/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using SText.Dialogs;
+
+namespace SText.Editor
+{
+    public class MainViewModel
+    {
+
+        public ReactiveCommand<Unit, Unit> About { get; }
+        public ReactiveCommand<Unit,Unit> OpenFile { get; }
+
+        public MainViewModel()
+        {
+            About = ReactiveCommand.Create(
+            () => {
+                new AboutDialog("1.3.90").Show();
+            });
+        }
+
+        
+        
+
+    }
+}
