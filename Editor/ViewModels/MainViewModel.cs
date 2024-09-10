3cffbe10d7491be93a82e3af981d4702c751cd4a
pahipy <pahipy@gmail.com>
2024-09-10 20:38:28 +0300

    adds content field

--- Editor/ViewModels/MainViewModel.cs
+++ Editor/ViewModels/MainViewModel.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Reactive;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveUI;
+using ReactiveUI.SourceGenerators;
 using SText.Dialogs;
 
 namespace SText.Editor
 {
-    public class MainViewModel
+    public class MainViewModel : ReactiveObject
     {
 
         public ReactiveCommand<Unit, Unit> About { get; }
         public ReactiveCommand<Unit,Unit> OpenFile { get; }
+
+
+        private string content = "";
+        public string Content
+        {
+            get => content;
+            set => this.RaiseAndSetIfChanged(ref content, value);
+        }
 
         public MainViewModel()
         {
@@ -24,8 +34,7 @@ namespace SText.Editor
             });
         }
 
-        
-        
+
 
     }
 }
