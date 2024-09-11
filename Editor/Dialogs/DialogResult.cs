8821671307531bd32c6620aa5e70f72c2743c223
pahipy <pahipy@gmail.com>
2024-09-11 21:23:35 +0300

    dialog result enum

---
+++ Editor/Dialogs/DialogResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor.Dialogs
+{
+    public enum DialogResult
+    {
+        OK,
+        Cancel,
+        Yes,
+        No,
+        Save,
+        DontSave
+    }
+}
