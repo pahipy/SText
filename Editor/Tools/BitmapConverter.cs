f2fe436aac73944709d18c97893f5dc4c0bc9739
pahipy <pahipy@gmail.com>
2024-09-26 21:51:30 +0300

    trying convert bitmap to bitmapimage; trying set source for an image

---
+++ Editor/Tools/BitmapConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SText.Tools
+{
+    public static class BitmapConverter
+    {
+        public static byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                bitmap.Save(memStream, System.Drawing.Imaging.ImageFormat.Png);
+                return memStream.ToArray();
+            }
+        }
+
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memStream = new MemoryStream(BitmapToByteArray(bitmap)))
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = memStream;
+                image.EndInit();
+                image.Freeze();
+                return image;
+            }
+        }
+    }
+}
