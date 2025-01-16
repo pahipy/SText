83f38fe305e8da74ed599a581d11f2b775ea1bb9
pahipy <pahipy@gmail.com>
2025-01-16 19:12:40 +0300

    Editor project was renamed as SText

--- SText/Properties/Resources.Designer.cs
+++ SText/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace SText {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SText.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
